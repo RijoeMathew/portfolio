80ac18a8a807109a35f070c1abe702ec3bd6ddd6
Rijoe Mathew <rijoe.c.mathew@gmail.com>
2023-06-24 16:26:03 -0400

    added projects

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,12 +1,10 @@
 from django.contrib import admin
 from django.urls import path,include
-from home import urls
-from about import urls
-from dashboards import urls
 
 urlpatterns = [
     path('', include('home.urls')),
     path('about', include('about.urls')), 
     path('dashboards', include('dashboards.urls')),
+    path('projects', include('projects.urls')),
     path('admin', admin.site.urls),
 ]
